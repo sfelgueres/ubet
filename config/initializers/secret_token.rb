ee53cfd0500361eb3fc0442cffefdcc244361508
Sergio Felgueres <sfelgueres@gmail.com>
2013-05-18 17:11:30 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ubet::Application.config.secret_token = 'e9d9f55aa53a8bab7c49d36958bd300bfe560d5d605dc7a1e28ee7e3eeb0aafea9f352f1d5aa6a4855a00a2b12388238b4fb37a0119a497a8045a462db0f370b'
