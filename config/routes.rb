f6ab18bfb821038b87f4d0347176900188fabcb9
Sergio Felgueres <sfelgueres@gmail.com>
2013-05-20 19:16:53 -0700

    Added Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Ubet::Application.routes.draw do
+  get 'about' => 'pages#about'
+  get 'login' => 'pages#login'
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
